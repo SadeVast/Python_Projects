d5503f367b1efef20a04c0c54cc11b64a05be1cb
VADESADE <vetalik.selikhov@mail.ru>
2023-05-29 11:01:35 +0300

    [upd] file

--- Chapter_5/Black_Jack.py
+++ Chapter_5/Black_Jack.py
@@ -17,24 +17,23 @@ BACKSIDE = 'backside'
 def main():
     print('''Blackjack, by Al Sweigart al@inventwithpython.com
 
-    Rules:
-      Try to get as close to 21 without going over.
-      Kings, Queens, and Jacks are worth 10 points.
-      Aces are worth 1 or 11 points.
-      Cards 2 through 10 are worth their face value.
-      (H)it to take another card.
-      (S)tand to stop taking cards.
-      On your first play, you can (D)ouble down to increase your bet
-      but must hit exactly one more time before standing.
-      In case of a tie, the bet is returned to the player.
-      The dealer stops hitting at 17.''')
+    Правила:
+      Постарайтесь подобраться как можно ближе к 21, не превышая значения.
+      Короли, дамы и валеты приносят 10 очков.
+      Тузы приносят 1 или 11 очков.
+      Карты со 2 по 10 имеют номинальную стоимость.
+      (H) взять другую карту.
+      (S) стоит перестать брать карты.
+      При первой игре вы можете (D)удвоить ставку, чтобы увеличить свою ставку.
+      В случае ничьей ставка возвращается игроку.
+      Дилер прекращает бить в 17.''')
 
     money = 5000
     while True:
         if money <= 0:
-            print("You're broke!")
-            print("Good thing you weren't playing with real money.")
-            print('Thanks for playing!')
+            print("Ты банкрот!")
+            print("Хорошо, что ты играл не на реальные деньги.")
+            print('Спасибо за игру')
             sys.exit()
 
         print('Money', money)
@@ -44,4 +43,154 @@ def main():
         dealerHand = [deck.pop(),deck.pop()]
         playerHand = [deck.pop(),deck.pop()]
         print('Bet:', bet)
-        
+
+        while True:
+            displayHands(playerHand, dealerHand, False)
+            print()
+
+            if getHandValue(playerHand) > 21:
+                break
+
+            move = getMove(playerHand, money - bet)
+
+            if move == 'D':
+                additionalBet = getBet(min(bet, (money - bet)))
+                bet += additionalBet
+                print('Bet increased to {}.'.format(bet))
+                print('Bet:', bet)
+
+            if move in ('H', 'D'):
+                newCard = deck.pop()
+                rank, suit = newCard
+                print('You drew a {} of {}.'.format(rank, suit))
+                playerHand.append(newCard)
+
+                if getHandValue(playerHand) > 21:
+                    continue
+
+            if move in ('S', 'D'):
+                break
+
+        if getHandValue(playerHand) <= 21:
+            while getHandValue(dealerHand) < 17:
+                print('Dealer hits...')
+                dealerHand.append(deck.pop())
+                displayHands(playerHand, dealerHand, False)
+
+                if getHandValue(dealerHand) > 21:
+                    break
+                input('Press Enter to continue...')
+                print('\n\n')
+
+        displayHands(playerHand, dealerHand, True)
+
+        playerValue = getHandValue(playerHand)
+        dealerValue = getHandValue(dealerHand)
+
+        if dealerValue > 21:
+            print('Дилерский бюст! Вы выиграли ${}!'.format(bet))
+            money += bet
+        elif (playerValue > 21) or (playerValue < dealerValue):
+            print('Ты проиграл(а)!')
+            money -= bet
+        elif playerValue > dealerValue:
+            print('Вы выиграли ${}!'.format(bet))
+            money += bet
+        elif playerValue == dealerValue:
+            print('Ничья, ставка возвращается к вам.')
+
+        input('Нажмите Enter, чтобы продолжить...')
+        print('\n\n')
+
+
+def getBet(maxBet):
+    while True:
+        print('Сколько вы ставите? (1-{} или QUIT)'.format(maxBet))
+        bet = input('> ').upper().strip()
+        if bet == 'QUIT':
+            print('Спасибо за игру!')
+            sys.exit()
+
+        if not bet.isdecimal():
+            continue
+
+        bet = int(bet)
+        if 1 <= bet <= maxBet:
+            return bet
+
+def getDeck():
+    deck = []
+    for suit in (HEARTS, DIAMONDS, SPADES, CLUBS):
+        for rank in range(2, 11):
+            deck.append((str(rank), suit))
+        for rank in ('J', 'Q', 'K', 'A'):
+            deck.append((rank, suit))
+    random.shuffle(deck)
+    return deck
+
+def displayHands(playerHand, dealerHand, showDealerHand):
+    print()
+    if showDealerHand:
+        print('DEALER:', getHandValue(dealerHand))
+        displayCards(dealerHand)
+    else:
+        print('DEALER: ???')
+        displayCards([BACKSIDE] + dealerHand[1:])
+
+    print('PLAYER:', getHandValue(playerHand))
+    displayCards(playerHand)
+
+def getHandValue(cards):
+    value = 0
+    numberOfAces = 0
+    for card in cards:
+        rank = card[0]
+        if rank == 'A':
+            numberOfAces += 1
+        elif rank in ('K', 'Q', 'J'):
+            value += 10
+        else:
+            value += int(rank)
+
+    value += numberOfAces
+    for i in range(numberOfAces):
+        if value + 10 <= 21:
+            value += 10
+
+    return value
+
+
+def displayCards(cards):
+    rows = ['', '', '', '', '']
+
+    for i, card in enumerate(cards):
+        rows[0] += ' ___  '
+        if card == BACKSIDE:
+            rows[1] += '|## | '
+            rows[2] += '|###| '
+            rows[3] += '|_##| '
+        else:
+            rank, suit = card
+            rows[1] += '|{} | '.format(rank.ljust(2))
+            rows[2] += '| {} | '.format(suit)
+            rows[3] += '|_{}| '.format(rank.rjust(2, '_'))
+
+    for row in rows:
+        print(row)
+
+def getMove(playerHand, money):
+
+    while True:
+        moves = ['(H)взять карту', '(S)остановиться']
+        if len(playerHand) == 2 and money > 0:
+            moves.append('(D)удвоить ставку')
+
+        movePrompt = ', '.join(moves) + '> '
+        move = input(movePrompt).upper()
+        if move in ('H', 'S'):
+            return move
+        if move == 'D' and '(D)удвоить ставку' in moves:
+            return move
+
+if __name__ == '__main__':
+    main()
