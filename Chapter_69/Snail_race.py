18a1738b4592fcdbd820bfdee27839bbba29ce33
SadeVast <vetalik.selikhov@mail.ru>
2023-08-11 16:45:21 +0300

    [add] new file and test branch

---
+++ Chapter_69/Snail_race.py
@@ -0,0 +1,62 @@
+import random, sys, time
+
+MAX_NUM_SNAILS = 8
+MAX_NAME_LENGTH = 20
+FINISH_LINE = 40
+
+print('''Snail Race, by Al Sweigart al@inventwithpython.com
+
+    @v <-- snail
+
+''')
+
+while True:
+    print('How many snails will race? Max:', MAX_NUM_SNAILS)
+    response = input('> ')
+    if response.isdecimal():
+        numSnailsRacing = int(response)
+        if 1 < numSnailsRacing <= MAX_NUM_SNAILS:
+            break
+    print('Enter a number between 2 and', MAX_NUM_SNAILS)
+
+snailNames = []
+for i in range(1, numSnailsRacing + 1):
+    while True:
+        print('Enter snail #' + str(i) + "'s name:")
+        name = input('> ')
+        if len(name) == 0:
+            print('Please enter a name.')
+        elif name in snailNames:
+            print('Choose a name that has not already been used.')
+        else:
+            break
+    snailNames.append(name)
+
+print('\n' * 40)
+print('START' + (' ' * (FINISH_LINE - len('START')) + 'FINISH'))
+print('|' + (' ' * (FINISH_LINE - len('|')) + '|'))
+snailProgress = {}
+for snailName in snailNames:
+    print(snailName[:MAX_NAME_LENGTH])
+    print('@v')
+    snailProgress[snailName] = 0
+
+time.sleep(1.5)
+while True:
+    for i in range(random.randint(1, numSnailsRacing // 2)):
+        randomSnailName = random.choice(snailNames)
+        snailProgress[randomSnailName] += 1
+        if snailProgress[randomSnailName] == FINISH_LINE:
+            print(randomSnailName, 'has won!')
+            sys.exit()
+
+    time.sleep(0.5)
+    print('\n' * 40)
+
+    print('START' + (' ' * (FINISH_LINE - len('START')) + 'FINISH'))
+    print('|' + (' ' * (FINISH_LINE - 1) + '|'))
+
+    for snailName in snailNames:
+        spaces = snailProgress[snailName]
+        print((' ' * spaces) + snailName[:MAX_NAME_LENGTH])
+        print(('.' * snailProgress[snailName]) + '@v')
---
+++ Chapter_70/Soroban.py
@@ -0,0 +1,128 @@
+NUMBER_OF_DIGITS = 10
+
+
+def main():
+    print('Soroban - The Japanese Abacus')
+    print('By Al Sweigart al@inventwithpython.com')
+    print()
+
+    abacusNumber = 0
+
+    while True:
+        displayAbacus(abacusNumber)
+        displayControls()
+
+        commands = input('> ')
+        if commands == 'quit':
+            break
+        elif commands.isdecimal():
+            abacusNumber = int(commands)
+        else:
+            for letter in commands:
+                if letter == 'q':
+                    abacusNumber += 1000000000
+                elif letter == 'a':
+                    abacusNumber -= 1000000000
+                elif letter == 'w':
+                    abacusNumber += 100000000
+                elif letter == 's':
+                    abacusNumber -= 100000000
+                elif letter == 'e':
+                    abacusNumber += 10000000
+                elif letter == 'd':
+                    abacusNumber -= 10000000
+                elif letter == 'r':
+                    abacusNumber += 1000000
+                elif letter == 'f':
+                    abacusNumber -= 1000000
+                elif letter == 't':
+                    abacusNumber += 100000
+                elif letter == 'g':
+                    abacusNumber -= 100000
+                elif letter == 'y':
+                    abacusNumber += 10000
+                elif letter == 'h':
+                    abacusNumber -= 10000
+                elif letter == 'u':
+                    abacusNumber += 1000
+                elif letter == 'j':
+                    abacusNumber -= 1000
+                elif letter == 'i':
+                    abacusNumber += 100
+                elif letter == 'k':
+                    abacusNumber -= 100
+                elif letter == 'o':
+                    abacusNumber += 10
+                elif letter == 'l':
+                    abacusNumber -= 10
+                elif letter == 'p':
+                    abacusNumber += 1
+                elif letter == ';':
+                    abacusNumber -= 1
+
+        if abacusNumber < 0:
+            abacusNumber = 0
+        if abacusNumber > 9999999999:
+            abacusNumber = 9999999999
+
+
+def displayAbacus(number):
+    numberList = list(str(number).zfill(NUMBER_OF_DIGITS))
+
+    hasBead = []
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '01234')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '56789')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '12346789')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '234789')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '034589')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '014569')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '012567')
+
+    for i in range(NUMBER_OF_DIGITS):
+        hasBead.append(numberList[i] in '01235678')
+
+    abacusChar = []
+    for i, beadPresent in enumerate(hasBead):
+        if beadPresent:
+            abacusChar.append('O')
+        else:
+            abacusChar.append('|')
+
+    chars = abacusChar + numberList
+    print("""
++================================+
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
+I  |  |  |  |  |  |  |  |  |  |  I
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
++================================+
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
+I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
++=={}=={}=={}=={}=={}=={}=={}=={}=={}=={}==+""".format(*chars))
+
+
+def displayControls():
+    print('  +q  w  e  r  t  y  u  i  o  p')
+    print('  -a  s  d  f  g  h  j  k  l  ;')
+    print('(Enter a number, "quit", or a stream of up/down letters.)')
+
+
+if __name__ == '__main__':
+    main()
